6687d589de4680143b1ea77ed9b1fe4597f868ca
Alex Saji <alexsaji111@gmail.com>
2021-05-19 20:00:21 +0530

    first commmit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('#test-species').owlCarousel({
+    rtl: false,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    loop: true,
+    nav: false,
+    dots: false,
+
+});
